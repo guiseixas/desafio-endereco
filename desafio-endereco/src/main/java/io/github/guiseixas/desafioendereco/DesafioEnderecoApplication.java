138762c79079158e752b1dd869cdf6387f624523
guilherme <guilherme.santos@dellead.com>
2023-03-19 21:44:07 -0300

    project structure

---
+++ desafio-endereco/src/main/java/io/github/guiseixas/desafioendereco/DesafioEnderecoApplication.java
@@ -0,0 +1,13 @@
+package io.github.guiseixas.desafioendereco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioEnderecoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioEnderecoApplication.class, args);
+	}
+
+}
---
+++ desafio-endereco/src/test/java/io/github/guiseixas/desafioendereco/DesafioEnderecoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.guiseixas.desafioendereco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioEnderecoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
