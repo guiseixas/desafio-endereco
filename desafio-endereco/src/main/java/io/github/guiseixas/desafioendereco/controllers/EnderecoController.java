541d749230b0cf0b4f6225a56a9bd94be7991759
guilherme <guilherme.santos@dellead.com>
2023-03-19 22:48:11 -0300

    method to validate CEP and get response in API VIA CEP

--- desafio-endereco/src/main/java/io/github/guiseixas/desafioendereco/controllers/EnderecoController.java
+++ desafio-endereco/src/main/java/io/github/guiseixas/desafioendereco/controllers/EnderecoController.java
@@ -1,15 +1,32 @@
 package io.github.guiseixas.desafioendereco.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.github.guiseixas.desafioendereco.entities.Endereco;
+import io.github.guiseixas.desafioendereco.services.dtos.ResponseCepDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/v1")
 public class EnderecoController {
 
-    @GetMapping
-    public String teste(){
-        return "Rota teste.";
+    @PostMapping("/consulta-endereco")
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseCepDTO consultaEndereco(@RequestBody Endereco endereco){
+        if(validarCEP(endereco.getCep())){
+            RestTemplate restTemplate = new RestTemplate();
+            ResponseEntity<ResponseCepDTO> response =
+                    restTemplate.getForEntity(String.format("https://viacep.com.br/ws/%s/json/", endereco.getCep()), ResponseCepDTO.class);
+            return response.getBody();
+        }
+        return null;
+    }
+
+    private static boolean validarCEP(String cep){
+        if(cep == null || cep.length() != 8){
+            return false;
+        }
+        return true;
     }
 }
---
+++ desafio-endereco/src/main/java/io/github/guiseixas/desafioendereco/services/dtos/ResponseCepDTO.java
@@ -0,0 +1,23 @@
+package io.github.guiseixas.desafioendereco.services.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseCepDTO {
+    private String cep;
+    private String logradouro;
+    private String complemento;
+    private String bairro;
+    private String localidade;
+    private String uf;
+    private String ibge;
+    private String gia;
+    private String ddd;
+    private String siafi;
+}
