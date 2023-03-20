71dcccfc2b8be74e1fb6efe11a52eab0cec9f5aa
guilherme <guilherme.santos@dellead.com>
2023-03-20 09:36:42 -0300

    remove unused imports

--- desafio-endereco/src/main/java/io/github/guiseixas/desafioendereco/controllers/EnderecoController.java
+++ desafio-endereco/src/main/java/io/github/guiseixas/desafioendereco/controllers/EnderecoController.java
@@ -7,16 +7,11 @@ import io.github.guiseixas.desafioendereco.services.dtos.ResponseCepDTO;
 import io.github.guiseixas.desafioendereco.services.dtos.ResponseToUserDTO;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.server.ResponseStatusException;
-
-import java.util.Objects;
 
 import static java.util.Objects.isNull;
-import static java.util.Objects.nonNull;
 
 @RestController
 @RequestMapping("/v1")
